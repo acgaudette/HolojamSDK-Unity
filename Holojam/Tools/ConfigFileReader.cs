b5dd9e1f2da094407d1a8ddea0b4907b1977471f
Gabriel B. Nunes <gabriel@kronopath.net>
2017-03-04 17:30:15 -0500

    Allow for setting of relay IP via an XML config file

---
+++ Holojam/Tools/ConfigFileReader.cs
@@ -0,0 +1,63 @@
+﻿// ConfigFileReader.cs
+// Created by Holojam Inc. on 03.04.17
+
+using UnityEngine;
+using System.Xml;
+using System.IO;
+using Holojam.Network;
+
+namespace Holojam.Tools {
+  public class ConfigFileReader : MonoBehaviour {
+    public string configFilePath = "./config.xml";
+
+    void Awake() {
+      LoadConfiguration();
+  }
+
+    void LoadConfiguration() {
+      XmlDocument configFile = new XmlDocument();
+      try {
+        configFile.Load(configFilePath);
+      }
+      catch (FileNotFoundException) {
+        Debug.LogWarning("Holojam configuration file at " + configFilePath + " not found.");
+        return;
+      }
+      catch (XmlException ex) {
+        Debug.LogWarning("Error reading Holojam configuration file: " + ex.Message);
+        return;
+      }
+
+      foreach (XmlNode node in configFile.DocumentElement.ChildNodes) {
+        switch (node.Name) {
+        case "RelayIP":
+          Client client = GetComponent<Client>();
+          if (client == null) {
+            // TODO: error
+            break;
+          }
+          string ip = GetText(node);
+          // TODO: maybe validate the build IP
+          client.ChangeRelayAddress(ip);
+          break;
+        case "BuildIndex":
+          // TODO
+          break;
+        default:
+          Debug.LogWarning("Unknown option \"" + node.Name
+                           + "\" found in Holojam config file at " + configFilePath);
+          break;
+        }
+      }
+  }
+
+    string GetText(XmlNode node) {
+      foreach (XmlNode child in node.ChildNodes) {
+        if (child.NodeType == XmlNodeType.Text) {
+          return child.Value;
+        }
+      }
+      return null;
+  }
+  }
+};
